75969a86151dd32bf69f99822bd95554c724a332
IzumIzBulki <118291913+IzumIzBulki@users.noreply.github.com>
2024-01-14 18:56:35 +0300

    Animal.java

---
+++ JJ_Task2/Animal.java
@@ -0,0 +1,22 @@
+package JavaJunior.JJ_Task2;
+/*
+Абстрактный класс Animal
+ */
+public abstract class Animal {
+    public String name;
+    public int age;
+
+    public Animal(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+}
